dd312d60ccd3a9101227b6197ff8d27ef8ea5d8b
WingChui <wingchui1226@gmail.com>
2017-08-07 23:25:46 +0800

    Update sprint3_API.py : adding /api/v1.0/prediction to get predictional price P.S. get predictional price by keyword is still in testing

--- sprint3_API.py
+++ sprint3_API.py
@@ -15,52 +15,50 @@ basic_auth = HTTPBasicAuth()
 token_auth = HTTPTokenAuth(scheme='Bearer')
 
 users = [
-    {'username': 'project', 'password': generate_password_hash('project')},
-    {'username': 'testing', 'password': generate_password_hash('testing')}
+	{'username': 'project', 'password': generate_password_hash('project')},
+	{'username': 'testing', 'password': generate_password_hash('testing')}
 ]
 
 # HTTPBasicAuth
 @basic_auth.verify_password
 def verify_password(username, password):
-    for user in users:
-        if user['username'] == username:
-            if check_password_hash(user['password'], password):
-                return True
-    return False
+	for user in users:
+		if user['username'] == username:
+			if check_password_hash(user['password'], password):
+				return True
+	return False
 
 @basic_auth.error_handler
 def unauthorized():
 	return make_response(jsonify( { 'error': 'Unauthorized access' } ), 401)
-	# return 403 instead of 401 can prevent browsers from displaying the default auth dialog
 
 #HTTPTokenAuth
 @token_auth.verify_token
 def verify_token(token):
-    #g.user = None
-    try:
-        data = serializer.loads(token)
-    except:
-        return False
-    if 'username' in data:
-        #g.user = data['username']
-        return True
-    return False
+	try:
+		data = serializer.loads(token)
+	except:
+		return False
+	if 'username' in data:
+		return True
+	return False
 	
 # Connection Test
-@app.route("/v1.0")
-@basic_auth.login_required
+@app.route("/")
 def check():
+	return "Connected to API server"
+
+# Connection Test
+@app.route("/api/v1.0")
+@basic_auth.login_required
+def check_basic_auth():
 	return "Connected to API server"
 	
 # Connection Test
 @app.route("/api/v1.0/token")
 @basic_auth.login_required
 def generate_token():
-	#users = ['John', 'Susan']
-	#for user in users:
-	#user = 'project'
 	token = serializer.dumps({'username': 'project'})
-		#print('Token for {}: {}\n'.format(user, token))
 	return jsonify({'token': token.decode('ascii'), 'duration': 1800})
 	
 @app.route("/api/v1.0/find_cheapest", methods=['GET'])
@@ -151,6 +149,123 @@ def price_data():
 	cnx.close()
 	
 	return jsonify({'distribution':results,'statistic':statistic})
+	
+@app.route("/api/v1.0/prediction", methods=['GET'])
+@token_auth.login_required
+def predict_data():
+	# Get parameters from call
+	category = request.args.get("category")
+	search = request.args.get("search")
+	# Optional parameters
+	source = request.args.get("source", default = "")
+	
+	if category is not None :
+		#Build a connection to DB
+		cnx = mysql.connector.connect(user = 'wing', password = 'qwert12345',host = 'localhost',database = 'project')
+		cursor = cnx.cursor()
+		
+		query = "SELECT predict_price FROM `predictionTest` WHERE category ='{}' AND predict_date = CURRENT_DATE+1".format(category)
+		cursor.execute(query)
+
+		for result in cursor :
+			prediction = result[0]
+
+		return jsonify({"category":category,"predict_price":prediction})
+
+	elif search is not None: 
+		query = "SELECT DISTINCT kind FROM test WHERE product LIKE '%{0}%' AND price in (SELECT MIN(price) as minimum FROM test WHERE product LIKE '%{0}%'".format(search)
+		if source is not "":
+			query += "AND source = '{}'".format(source)
+		query += ") Limit 5"
+		cursor.execute(query)
+		possibleCat = [i for i in cursor]
+		category = possibleCat[0]
+		suggestion = possibleCat[1:]
+		
+		query = "SELECT predict_price FROM `predictionTest` WHERE category ='{}' AND predict_date = CURDATE()+1".format(category)
+		cursor.execute(query)
+		
+		prediction = [result for result in cursor]
+		
+		output = {"category":category,"predict_price":prediction,"suggest_type":suggestion}
+		
+		return jsonify(output)
+
+# API for SPRINT 2
+@app.route("/api/find_cheapest", methods=['GET'])
+def sprint2_find_cheapest():
+	#Get parameter from call
+	if request.args.get("kind") is None : 
+		return 'parameter "kind" is missing !'
+	else : 
+		kind = request.args.get("kind")
+	
+	#Build a connection to DB
+	cnx = mysql.connector.connect(user = 'wing', password = 'qwert12345',host = 'localhost',database = 'project')
+	cursor = cnx.cursor()
+
+	#Send query to Get at most 10 item's data with specificed kind which provide lowest price
+	query = (("SELECT product, price, url, website, picture, update_time FROM test WHERE kind='%s' AND price IN (SELECT MIN(price) AS minimum FROM test WHERE kind = '%s') LIMIT 10") % (kind, kind))
+	cursor.execute(query)
+	
+	#Get data from cursor
+	results = []
+	for (product, price, url, website, picture, update_time) in cursor :
+		update_time = ("{:%d %b %Y}").format(update_time)
+		row = dict(zip(("p_name", "price", "url", "source", "imagePath", "update_time"),(product, price, url, website, picture, update_time)))
+		results.append(row)
+		
+	#Close object
+	cursor.close()
+	cnx.close()
+	
+	#Produce and return JSON array
+	return jsonify(results)
+
+@app.route("/api/price_data", methods=['GET'])
+def sprint2_price_data():
+	#Get parameters from call
+	if request.args.get("kind") is None : 
+		return 'parameter "kind" is missing !'
+	else : 
+		kind = request.args.get("kind")
+
+	unit = request.args.get("unit", default = 100, type = int)
+	min = request.args.get("min", default = 0, type = int)
+	max = request.args.get("max", default = 1000, type = int)
+	
+	#Build a connection to DB
+	cnx = mysql.connector.connect(user = 'wing', password = 'qwert12345',host = 'localhost',database = 'project')
+	cursor = cnx.cursor()
+
+	#Develop a list of data for query execution
+	query_list = []
+	lower_limit_list = range(max + 1)[min : (max+1) : unit]
+	region_num = len(lower_limit_list)
+	for i in lower_limit_list :
+		query_list.append(i)
+		if i != max :
+			query_list.append(i+unit)
+		query_list.append(i)
+
+	query_list.append(kind)
+	
+	#Send query to DB
+	sql_string = "SELECT COUNT(*) AS num , CASE " + "WHEN price >= %d AND price < %d THEN %d " * (region_num - 2) + "WHEN price BETWEEN %d AND %d THEN %d WHEN price > %d THEN %d END AS price_lower_limit FROM test WHERE kind = '%s' GROUP BY price_lower_limit"
+
+	query = (sql_string % tuple(query_list))
+	cursor.execute(query)
+	
+	#Take out the data from cursor
+	results = {}
+	for num in cursor : 
+		results[num[1]] = num[0]
+	
+	#Close object
+	cursor.close()
+	cnx.close()
+	
+	return jsonify(results)
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 if __name__ == "__main__" :
